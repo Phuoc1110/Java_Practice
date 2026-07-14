7430a52aa3a7db8a5b98cf4b09a4450eb157f8e5
Phuoc1110 <phuoc11102004@gmail.com>
2026-07-14 16:09:55 +0700

    Lesson 25

--- src/com/NoVC_21_30/Lesson24.java
+++ src/com/NoVC_21_30/Lesson24.java
@@ -9,8 +9,12 @@ import javax.swing.*;
 
 public class Lesson24 extends JFrame {
 	
-	JComboBox<String> favoriteShows;
+	JList<String> favoriteShows;
 	JButton button1;
+	JList<String> favoriteMovies, favoriteColors;
+	DefaultListModel defListModel = new DefaultListModel();
+	JScrollPane scroller;
+	
 	String infoOnComponent;
 	
 	public static void main(String[] args) {
@@ -30,40 +34,55 @@ public class Lesson24 extends JFrame {
 		
 		String[] shows = {"Breaking Bad", "Doctor Who", "Interstella"};
 		
-		favoriteShows = new JComboBox<String>();
+		favoriteShows = new JList<String>(shows);
 		
-		favoriteShows.addItem("Devil May Cry");
-		favoriteShows.addItem(shows[0]);
-		favoriteShows.addItem("?");
+		favoriteShows.setFixedCellHeight(30);
+		
+		favoriteShows.setFixedCellWidth(150);
+		
+		favoriteShows.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 		
 		thePanel.add(favoriteShows);
 		
+		String[] colors = {"Color 1", "Color 2", "Color 3", "Color 4", "Color 5", "Color 6", "Color 7"};
+		
+		for (String color : colors) {
+			defListModel.addElement(color);
+		}
+		
+		defListModel.add(2,"Purple");
+		
+		JList<String> favoriteColors = new JList<String>(defListModel);
+		
+		favoriteColors.setFixedCellHeight(30);
+		favoriteColors.setFixedCellWidth(150);
+		
+		favoriteColors.setVisibleRowCount(4);
+		
+		scroller = new JScrollPane(favoriteColors, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		
+		thePanel.add(scroller);
+		
 		button1 = new JButton("Get Answer");
 		
-		button1.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				if (arg0.getSource() == button1) {
-					
-					favoriteShows.setEditable(true);
-					
-					infoOnComponent += "Item at 0 :" + favoriteShows.getItemAt(0) + "\n";
-					
-					infoOnComponent += "Num of shows: " + favoriteShows.getItemCount() + "\n";
-					
-					infoOnComponent += "Selected ID :" + favoriteShows.getSelectedIndex() + "\n";
-					
-					infoOnComponent += "Selected Value: " + favoriteShows.getSelectedItem() + "\n";
-					
-					infoOnComponent += "Are editable: " + favoriteShows.isEditable() + "\n";
-					
-					JOptionPane.showMessageDialog(Lesson24.this, infoOnComponent, "Information", JOptionPane.INFORMATION_MESSAGE);
-				}
+		button1.addActionListener(e -> {
+			if (e.getSource() == button1) {
+				if (defListModel.contains("Purple")) infoOnComponent += "Purple is there\n";
 				
+				if (defListModel.isEmpty()) infoOnComponent += "Nothing is here\n";
+				
+				infoOnComponent += "Element in the list: " + defListModel.getSize() + "\n";
+				
+				infoOnComponent += "Last element: " + defListModel.lastElement() + "\n";
+				
+				infoOnComponent += "First element: " + defListModel.firstElement() + "\n";
+				
+				JOptionPane.showMessageDialog(Lesson24.this, infoOnComponent, "Answer", JOptionPane.INFORMATION_MESSAGE);
+				
+				infoOnComponent = "";
 			}
 		});
-		
+				
 		thePanel.add(button1);
 		
 		this.add(thePanel);
